a8b3c2028de43931733e4e5bad4cc60226181760
Tsubasa <lixuesonlu@qq.com>
2021-05-03 16:54:56 +0800

    change []interface{} TO api.Info

--- api/dynamic.go
+++ api/dynamic.go
@@ -7,38 +7,47 @@ import (
 	"github.com/tsubasa597/BILIBILI-HELPER/global"
 )
 
+type Info struct {
+	T    int32
+	Err  error
+	Card interface{}
+}
+
 // GetDynamicMessage 获取目标 uid 的第一条记录
-//
-// return : 时间戳、Card 实例、错误信息
-func GetDynamicMessage(hostUID int64) (int32, interface{}, error) {
+func GetDynamicMessage(hostUID int64) Info {
 	dynamicSvrSpaceHistoryResponse, err := GetDynamicSrvSpaceHistory(hostUID)
 	if err != nil {
-		return -1, nil, err
+		return Info{
+			T:   -1,
+			Err: err,
+		}
 	}
 
 	if dynamicSvrSpaceHistoryResponse.Code != 0 {
-		return -1, nil, fmt.Errorf("请求发生错误: " + dynamicSvrSpaceHistoryResponse.Message)
+		return Info{
+			T:   -1,
+			Err: fmt.Errorf("请求发生错误: " + dynamicSvrSpaceHistoryResponse.Message),
+		}
 	}
 
-	c, e := GetOriginCard(dynamicSvrSpaceHistoryResponse.Data.Cards[0])
-	if err != nil {
-		return -1, nil, e
-	}
-	return c[0].(int32), c[1], nil
+	return GetOriginCard(dynamicSvrSpaceHistoryResponse.Data.Cards[0])
 }
 
 // GetOriginCard 获取 Card 的源动态
-//
-// return []interface{} : 时间戳、Card 实例、错误信息
-func GetOriginCard(c *Card) ([]interface{}, error) {
+func GetOriginCard(c *Card) (info Info) {
+	info = Info{
+		T: c.Desc.Timestamp,
+	}
 	switch c.Desc.Type {
 	case 0:
-		return []interface{}{-1, nil}, fmt.Errorf("未知动态")
+		info.Err = fmt.Errorf("未知动态")
+		return
 	case 1:
 		dynamic := &CardWithOrig{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
 		return GetOriginCard(&Card{
@@ -51,77 +60,96 @@ func GetOriginCard(c *Card) ([]interface{}, error) {
 		dynamic := &CardWithImage{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 4:
 		dynamic := &CardTextOnly{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 8:
 		dynamic := &CardWithVideo{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 64:
 		dynamic := &CardWithPost{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 256:
 		dynamic := &CardWithMusic{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 512:
 		dynamic := &CardWithAnime{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 2048:
 		dynamic := &CardWithSketch{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 4200:
 		dynamic := &CardWithLive{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	case 4308:
 		dynamic := &CardWithLiveV2{}
 		err := json.Unmarshal([]byte(c.Card), dynamic)
 		if err != nil {
-			return []interface{}{-1, nil}, err
+			info.Err = err
+			return
 		}
 
-		return []interface{}{c.Desc.Timestamp, dynamic}, nil
+		info.Card = dynamic
+		return
 	}
 
-	return []interface{}{-1, nil}, fmt.Errorf("解析错误")
+	info.Err = fmt.Errorf("解析错误")
+	return
 }
 
 // GetDynamicSrvSpaceHistory 获取目的 uid 的所有动态
--- dynamic/dynamic.go
+++ dynamic/dynamic.go
@@ -9,29 +9,32 @@ import (
 )
 
 type DynamicListen struct {
-	C      chan []interface{}
-	ctx    context.Context
-	cancel context.CancelFunc
-	ups    map[int64]context.CancelFunc
+	T             *time.Ticker
+	C             chan api.Info
+	ctx           context.Context
+	cancel        context.CancelFunc
+	upsCancelFunc map[int64]context.CancelFunc
+	upsContext    map[int64]context.Context
 }
 
 func (d *DynamicListen) ListenDynamic(uid int64) context.Context {
-	if _, ok := d.ups[uid]; ok {
-		return nil
+	if _, ok := d.upsCancelFunc[uid]; ok {
+		return d.upsContext[uid]
 	}
 
 	ct, cl := context.WithCancel(d.ctx)
-	d.ups[uid] = cl
-	go listen(uid, ct, d.C)
+	d.upsContext[uid] = ct
+	d.upsCancelFunc[uid] = cl
+	go listen(*d.T, uid, ct, d.C)
 	return ct
 }
 
 func (d *DynamicListen) StopListenUP(uid int64) error {
-	if _, ok := d.ups[uid]; !ok {
+	if _, ok := d.upsCancelFunc[uid]; !ok {
 		return fmt.Errorf("该用户未监听")
 	}
 
-	d.ups[uid]()
+	d.upsCancelFunc[uid]()
 	return nil
 }
 
@@ -40,14 +43,13 @@ func (d DynamicListen) Stop() {
 	d.cancel()
 }
 
-func listen(uid int64, ctx context.Context, ch chan<- []interface{}) {
+func listen(ticker time.Ticker, uid int64, ctx context.Context, ch chan<- api.Info) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case <-time.NewTicker(time.Minute).C:
-			t, c, e := api.GetDynamicMessage(uid)
-			ch <- []interface{}{uid, t, c, e}
+		case <-ticker.C:
+			ch <- api.GetDynamicMessage(uid)
 		}
 	}
 }
@@ -55,9 +57,11 @@ func listen(uid int64, ctx context.Context, ch chan<- []interface{}) {
 func New() *DynamicListen {
 	c, cl := context.WithCancel(context.Background())
 	return &DynamicListen{
-		C:      make(chan []interface{}, 1),
-		ctx:    c,
-		cancel: cl,
-		ups:    make(map[int64]context.CancelFunc),
+		T:             time.NewTicker(time.Minute),
+		C:             make(chan api.Info, 1),
+		ctx:           c,
+		cancel:        cl,
+		upsCancelFunc: make(map[int64]context.CancelFunc),
+		upsContext:    make(map[int64]context.Context),
 	}
 }
